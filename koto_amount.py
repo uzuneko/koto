b1460cdc1d35a26ac9b8959a27986f6e6d6870b6
uzuneko <35770202+uzuneko@users.noreply.github.com>
2018-02-28 23:42:16 +0900

    KOTO tools
    トランザクションログからamountを集計

--- koto_amount.py
+++ koto_amount.py
@@ -4,29 +4,30 @@
 import os.path
 import glob
 import json
+import datetime
 
 hdir = os.path.expanduser('~')
 total=0
 cnt=0
 
 fl = sorted(glob.glob(os.path.join(hdir, '.koto/[1-9]*.json')))
-for fn in fl:
+for fp in fl:
   subtotal = 0
   subcnt = 0
 
-  f = open(fn, 'r')
+  fn, ext = os.path.splitext(os.path.basename(fp))
+  dt = datetime.datetime.fromtimestamp(float(fn))
 
+  f = open(fp, 'r')
   data = json.load(f)
-  
-
   for attr in data:
     subcnt += 1
     subtotal = subtotal + attr.get('amount')
 
-  print '{0}: {1:4d}件: {2:13.8f}'.format(fn, subcnt, subtotal)
+  print '{0}: {1:4d}件: {2:13.8f}'.format(dt, subcnt, subtotal)
   f.close
   total = total + subtotal
   cnt = cnt + subcnt
 
-print "-" * 60
-print " " * 22 + u'総件数: {0:4d}件   総合計:{1:13.8f} koto'.format(cnt, total)
+print "-" * 43
+print " " * 21 + '{0:4d}件: {1:13.8f} koto'.format(cnt, total)
